8aa4652a28069fa83fe667537af3c9cf7d563246
Tomi Paul <tomipaul95@gmail.com>
2022-02-06 04:31:02 +0100

    fetch list of branches

--- .github/actions/addFixVersion.js
+++ .github/actions/addFixVersion.js
@@ -1,5 +1,14 @@
 const run = async () => {
   console.log(process.env.GITHUB_REF_NAME)
+  console.log(process.env.GITHUB_API_URL)
+  const response = await fetch(`${process.env.GITHUB_API_URL}/repos/tomipaul/CollegeBoard/branches`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      accept: 'application/vnd.github.v3+json'
+    },
+  })
+  console.log('response', response)
 };
 
 run();
