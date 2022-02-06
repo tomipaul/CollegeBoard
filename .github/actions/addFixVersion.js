adbf8a5e5a59db7f17c0aae07bff1a1cebb95d59
Tomi Paul <tomipaul95@gmail.com>
2022-02-06 05:00:31 +0100

    Use axios

--- .github/actions/addFixVersion.js
+++ .github/actions/addFixVersion.js
@@ -1,7 +1,11 @@
+const axios = require('axios');
+
 const run = async () => {
   console.log(process.env.GITHUB_REF_NAME)
   console.log(process.env.GITHUB_API_URL)
-  const response = await fetch(`${process.env.GITHUB_API_URL}/repos/tomipaul/CollegeBoard/branches`, {
+  const response = await axios.request({
+    baseURL: `${process.env.GITHUB_API_URL}`,
+    url: '/repos/tomipaul/CollegeBoard/branches',
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
