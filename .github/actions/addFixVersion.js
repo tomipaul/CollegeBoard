f1da3290fb1a72d2399efb8d0af5443bc79fa54f
Tomi Paul <tomipaul95@gmail.com>
2022-02-07 05:09:55 +0100

    Use @actions/github

--- .github/actions/addFixVersion.js
+++ .github/actions/addFixVersion.js
@@ -1,17 +1,32 @@
-const axios = require('axios');
+const github = require('@actions/github');
+
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+
+const getBranch = async (branch) => {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
+  try {
+    const response = await octokit.rest.repos.getBranch({
+      owner,
+      repo,
+      branch
+    });
+    return response
+  } catch (e) {
+    console.log(JSON.stringify(e))
+  }
+};
 
 const run = async () => {
-  console.log(process.env.GITHUB_REF_NAME)
-  console.log(process.env.GITHUB_API_URL)
-  const response = await axios.request({
-    baseURL: `${process.env.GITHUB_API_URL}`,
-    url: '/repos/tomipaul/CollegeBoard/branches',
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      accept: 'application/vnd.github.v3+json'
-    },
-  })
+  // const response = await axios.request({
+  //   baseURL: `${process.env.GITHUB_API_URL}`,
+  //   url: '/repos/tomipaul/CollegeBoard/branches',
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     accept: 'application/vnd.github.v3+json'
+  //   },
+  // })
+  const response = await getBranch('master')
   console.log('response', response)
 };
 
