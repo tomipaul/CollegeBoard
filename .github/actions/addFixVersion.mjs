feb241ea94f5a4bbedde64ad0d62d8bbb1ea243f
Tomi Paul <tomipaul95@gmail.com>
2022-02-08 03:56:01 +0100

    Remove unneccessary console statement 1

--- .github/actions/addFixVersion.mjs
+++ .github/actions/addFixVersion.mjs
@@ -67,7 +67,6 @@ const run = async () => {
   const event = github.context
   console.log('event', event.payload.commits)
   console.log(event.payload.commits.length)
-  console.log('just for the second test commit')
 
   const releaseVersions = await getUnreleasedVersions()
   console.log('release versions', releaseVersions)
